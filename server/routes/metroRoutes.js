eb99cb6282d158d2fe0dd16625a7182a1ac18fcd
djlee <djlee@78lab.com>
2024-11-05 17:13:16 +0900

    timetable train

--- server/routes/metroRoutes.js
+++ server/routes/metroRoutes.js
@@ -12,15 +12,29 @@ router.get('/timetable', async (req, res) => {
     let fr_code = req.query.fr_code;
     let inout_tag = req.query.inout_tag;
     let week_tag = req.query.week_tag;
+    let train_no = req.query.train_no;
+    let query = {}
+    let sort = {}
+    if (fr_code) { query.FR_CODE = fr_code; }
+    if (inout_tag) { query.INOUT_TAG = inout_tag; }
+    if (week_tag) { query.WEEK_TAG = week_tag; }
+    if (train_no) { 
+      query.TRAIN_NO = train_no; 
+      if(inout_tag == 1) sort.FR_CODE = -1;
+      else sort.FR_CODE = 1; 
+    }
+
+    
     // let subwayNm = req.query.subwayNm;
     // let realtimePosUrl = `${SEOUL_BASE_URL}/${SEOUL_SERVICE_KEY}/json/realtimePosition/0/100/${subwayNm}`
     try {
         // Fetch data from the external API
-        console.log(fr_code)
+        console.log("query:",query,"sort:",sort)
+        // console.log(`query:${query} sort:${sort}`) 
         // const realtimePositionResponse = await axios.get(realtimePosUrl);
         // const realtimePositionData = realtimePositionResponse.data;
         // const trainList = realtimePositionData.realtimePositionList
-        const timetable = await TimeTable.find({ FR_CODE: fr_code, INOUT_TAG: inout_tag, WEEK_TAG: week_tag });
+        const timetable = await TimeTable.find(query).sort(sort);
         // const  = await Train.find({ subwayId: subwayId });
         res.json(timetable);
 
@@ -44,7 +58,7 @@ router.get('/stations', async (req, res) => {
         // const realtimePositionResponse = await axios.get(realtimePosUrl);
         // const realtimePositionData = realtimePositionResponse.data;
         // const trainList = realtimePositionData.realtimePositionList
-        const stations = await MetroStation.find({ subwayId: subwayId });
+        const stations = await MetroStation.find({ subwayId: subwayId }).sort({ statnId: 1 });
         // const  = await Train.find({ subwayId: subwayId });
         res.json(stations);
 
